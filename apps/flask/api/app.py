9a608622d3741e8662311ceb8136c7b0891ec270
Alexander <surfmetrics@gmail.com>
2023-04-26 18:35:37 +0200

    added text completion and cleaned up app

--- apps/flask/api/app.py
+++ apps/flask/api/app.py
@@ -1,17 +1,25 @@
 from flask import Blueprint, render_template, request
+import ai21
 
+ai21.api_key = 'MWoMUJbSC3GgrEKx51opEooZW1OGAdOL'
 api = Blueprint("api", __name__, template_folder='templates', static_folder='static', static_url_path='api/static')
 
-@api.route("/", methods=["GET"])
-def home():
+# @api.route("/", methods=["GET"])
+# def home():
     
-    words = get_words('api/snowwhite.txt')
-    write_words(words)
+#     words = get_words('api/snowwhite.txt')
+#     write_words(words)
     
-    return render_template("index.html", first_part=words)
+#     return render_template("index.html", first_part=words)
 
-@api.route("/select", methods=["POST"])
+@api.route("/", methods=["GET", "POST"])
 def select():
+    if request.method == "GET":
+        words = get_words('api/snowwhite.txt')
+        write_words(words)
+    
+        return render_template("index.html", first_part=words)
+    
     print(request.form, flush=True)
     
     if request.form.get('word'):
@@ -35,11 +43,26 @@ def select():
         return render_template("index.html", first_part=words, add_text=True)
     
     if request.form.get('submit-text'):
-        text = request.form.get('text-input')
-        
-        with open('api/snowwhite-alternative.txt', 'a') as f:
-            f.write(text)
+        added = request.form.get('text-input')
     
+        print(added)
+        with open('api/snowwhite-alternative.txt', 'r') as f:
+            original = f.read()
+        
+        if added:
+            text = original.strip() + ' ' + added.strip()
+        else:
+            text = original.strip()
+            
+        completion = complete(text)
+        print(text)
+        print('------')
+        print(completion)
+        with open('api/snowwhite-alternative.txt', 'a') as f:
+            f.write(added)
+            f.write(completion)
+        
+        # words = total.split(' ')
         words = get_words('api/snowwhite-alternative.txt')
         
         return render_template("index.html", first_part=words, add_text=False)
@@ -64,3 +87,14 @@ def write_words(words: list) -> None:
             f.write(word)
             if not word == '\n':
                 f.write(' ')
+                
+def complete(text: str) -> str:
+    response = ai21.Completion.execute(
+        model='j2-large',
+        prompt=text,
+        temperature=0.5,
+        minTokens=50,
+        maxTokens=100,
+        numResults=1
+    )
+    return response.completions[0].data.text.strip()
